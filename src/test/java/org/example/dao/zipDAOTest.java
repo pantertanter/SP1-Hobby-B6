6a8a7f5ad4975d7c0ec7d8081eaae5534cfd978a
SebastianMandrup <46905237+SebastianMandrup@users.noreply.github.com>
2023-09-05 09:27:20 +0200

    lidt dao test

---
+++ src/test/java/org/example/dao/zipDAOTest.java
@@ -0,0 +1,44 @@
+package org.example.dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class zipDAOTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void saveZip() {
+    }
+
+    @Test
+    void readZip() {
+    }
+
+    @Test
+    void readAllZips() {
+    }
+
+    @Test
+    void updateZip() {
+    }
+
+    @Test
+    void deleteZip() {
+    }
+}
